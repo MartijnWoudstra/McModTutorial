0959c17b843d8b2426292835e8d241b27995e015
MartijnWoudstra <martijnhenriwoudstra@gmail.com>
2014-03-17 19:57:40 +0100

    Tutorial 3, Backbone Block

---
+++ src/main/java/mcmodtutorial/blocks/BlockMcModTutorial.java
@@ -0,0 +1,74 @@
+package mcmodtutorial.blocks;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mcmodtutorial.lib.References;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+/**
+ * mcmodtutorial
+ * <p/>
+ * BlockMcModTutorial
+ *
+ * @Author Martijn Woudstra
+ */
+
+public class BlockMcModTutorial extends Block
+{
+    /**
+     * Constructor for when no material is passed on.
+     * Default material: rock
+     */
+    public BlockMcModTutorial()
+    {
+        super(Material.rock);
+    }
+
+    /**
+     * Constructor for defined material.
+     * @param material
+     */
+    public BlockMcModTutorial(Material material)
+    {
+        super(material);
+    }
+
+    /**
+     * Gets the unwrapped name of a block
+     * UnlocalizedName is usually something like tile.[modid].[blockname]
+     * This method only returns the blockname
+     * @param unlocalizedName
+     * @return blockname
+     */
+    public String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+    /**
+     * Overrides the block method for returning unlocalizedName.
+     * This way, when you extend this class, everything is handled properly.
+     * @return unlocalizedName
+     */
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", References.RESOURCESPREFIX, getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    /**
+     * Overrides the registerBlockIcon method.
+     * This method handles all the textures.
+     * Call registerIcon() and pass it a
+     * Format: [modid]:[blockname]
+     * @param iconRegister
+     */
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        this.blockIcon = iconRegister.registerIcon(References.RESOURCESPREFIX + getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+}
--- src/main/java/mcmodtutorial/lib/References.java
+++ src/main/java/mcmodtutorial/lib/References.java
@@ -10,9 +10,10 @@ package mcmodtutorial.lib;
 
 public class References
 {
-    public static final String MODID = "mcmodtutorial";
-    public static final String MODNAME = "Mc Mod Tutorial";
-    public static final String VERSION = "1.0.0";
+    public static final String MODID               = "mcmodtutorial";
+    public static final String MODNAME             = "Mc Mod Tutorial";
+    public static final String VERSION             = "1.0.0";
     public static final String CLIENTPROXYLOCATION = MODID + ".core.proxy.ClientProxy";
     public static final String COMMONPROXYLOCATION = MODID + ".core.proxy.CommonProxy";
+    public static final String RESOURCESPREFIX     = MODID + ":";
 }
